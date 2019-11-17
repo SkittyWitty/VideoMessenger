fecae8813846322d151abe4aece91145a9b7e81f
Mindy DeWaal <kawiititan@gmail.com>
2019-11-16 18:19:59 -0800

    Initial commit with base Winsock

---
+++ VideoMessenger/VideoMessenger.cpp
@@ -0,0 +1,23 @@
+﻿// VideoMessenger.cpp : Defines the entry point for the application.
+//
+
+#include "VideoMessenger.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	return 0;
+}
